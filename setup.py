0880c8906a48b491bc38478a563c330638fdf7ac
Ross McFarland <rwmcfa1@gmail.com>
2022-02-02 13:32:11 -0800

    setup.py requirements & Stale action tmpl updates

--- setup.py
+++ setup.py
@@ -17,23 +17,34 @@ def version():
 
 description, long_description = descriptions()
 
+tests_require = (
+    'pytest',
+    'pytest-network',
+)
+
 setup(
     author='Ross McFarland',
     author_email='rwmcfa1@gmail.com',
     description=description,
+    extras_require={
+        'dev': tests_require + (
+            'build>=0.7.0',
+            'pycodestyle>=2.6.0',
+            'pyflakes>=2.2.0',
+            'readme_renderer[md]>=26.0',
+            'twine>=3.4.2',
+        ),
+    },
+    install_requires=(
+        'octodns>=0.9.14',
+    ),
     license='MIT',
     long_description=long_description,
     long_description_content_type='text/markdown',
     name='octodns-etchosts',
     packages=find_packages(),
     python_requires='>=3.6',
-    install_requires=(
-        'octodns>=0.9.14',
-    ),
+    tests_require=tests_require,
     url='https://github.com/octodns/octodns-etchosts',
     version=version(),
-    tests_require=(
-        'pytest',
-        'pytest-network',
-    ),
 )
